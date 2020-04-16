dc0be932d6d4bc01304220dc207b4a5c40e3cf01
iclare <iandillonclare@gmail.com>
2020-04-15 23:08:55 -0400

    Read glob patterns

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    /// Verbose mode
+    #[structopt(short, long)]
+    verbose: bool,
+    /// Files to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    let verbose: bool = args.verbose;
+    let files: &Vec<PathBuf> = &args.files;
+
+    for file in files {
+        if verbose {
+            println!("{:?}", file);
+        }
+        let file_stem = match file.file_stem() {
+            Some(file_stem) => file_stem,
+            None => {
+                eprintln!("error: not a file {:?}", file);
+                std::process::exit(1);
+            }
+        };
+    }
 }
